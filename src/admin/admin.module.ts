c4f42d0fc2f0e7c6bd0bf402ff1814ceac34cae7
unknown <monnkkey2004@gmail.com>
2025-10-30 22:37:03 +0700

    fix

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 // Để sử dụng guards
 
-import { PrismaModule } from 'sys/prisma/prisma.module';
-import { GuardModule } from 'shared/guards/guard.module';
+import { PrismaModule } from '../sys/prisma/prisma.module';
+import { GuardModule } from '../shared/guards/guard.module';
 import { UsersModule } from './users/user.module';
 import { MoviesModule } from './movies/movie.module';
 import { BookingsModule } from './bookings/booking.module';
-import { TokenModule } from 'sys/token/token.module';
+import { TokenModule } from '../sys/token/token.module';
 
 @Module({
   imports: [
